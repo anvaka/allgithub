a2a980b59bfb49632dcb4ed5260090cd404598eb
anvaka <anvaka@gmail.com>
2015-05-13 00:52:56 -0700

    Basic indexer

---
+++ index.js
@@ -0,0 +1,42 @@
+var githubClient = require('./lib/githubClient.js')(process.env.GH_TOKEN);
+var redisClient = require('./lib/redisClient.js')();
+
+redisClient.getLastSavedId()
+  .then(greetUser)
+  .then(indexUsers);
+
+function greetUser(since) {
+  console.log('Welcome to the github users crawler!');
+  if (since) {
+    since = parseInt(since, 10);
+    console.log('Attemtpting to resume indexing since user id: ' + since);
+  }
+
+  return since;
+}
+
+function indexUsers(since) {
+  githubClient.getUsers(since)
+    .then(save)
+    .then(loadMore)
+    .catch(function(e) {
+      console.log('Something went bad: ' + e);
+      console.log('Quiting...');
+      process.exit(-1);
+    });
+}
+
+function loadMore(ctx) {
+  if (ctx.isDone) return;
+  indexUsers(ctx.lastSavedId);
+}
+
+function save(users) {
+  var lastSavedId = redisClient.save(users);
+  console.log('last saved id: ' + lastSavedId);
+
+  return {
+    isDone: users.length < 100, // this can only happen if we reached the last page
+    lastSavedId: lastSavedId
+  };
+}
